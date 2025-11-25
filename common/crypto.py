343c1fb8b1f6d813bbcb7efafc0c839de3de17d8
chfhm <chahinez.fahmi@gmail.com>
2025-11-25 14:56:36 +0100

    cryptographie

---
+++ common/crypto.py
@@ -0,0 +1,35 @@
+import math
+import random
+
+
+
+def is_prime(n):
+    """verifie si un nombre est premier"""
+    if n <= 1:
+        return False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+
+def generate_keys():
+    """génère une paire de clés publique et privée"""
+    p = 0
+    q = 0
+    while not is_prime(p):
+        p = random.randint(11, 50)
+    while not is_prime(q) or q == p:
+        q = random.randint(11, 50)
+
+    n = p * q
+    phi = (p - 1) * (q - 1)
+
+    e = 3
+    while math.gcd(e, phi) != 1:
+        e += 2
+
+    d = pow(e, -1, phi)
+    return ((e, n), (d, n))
+
