2fb1b6f6f2419456034212b6eefe126bdc4901a5
root <root@debian1.myguest.virtualbox.org>
2025-12-31 05:40:14 +0100

    update clientB a mettre sur debian2

--- client/clientB.py
+++ client/clientB.py
@@ -1,1 +1,9 @@
 #ce code est a mettre sur la machine debian 2 clientB.py 
+
+from common.network import start_server, send_message
+def handle_bob(msg):
+    print(f"MESSAGE RECU : {msg}")
+    return "Recu par Bob"
+print("Client B (Bob) en attente sur port 8888...")
+start_server(8888, handle_bob)
+ 
